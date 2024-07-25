63c946b9fe10042e0e2172a2f821a79bc92b7095
Praveen Katha <praveenkatha16@gmail.com>
2024-07-25 12:48:49 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+const App = () => (
+	<div className="w-full h-full flex bg-slate-700 justify-center items-center">
+		<h1 className="text-teal-300 font-bold text-2xl">
+			React.TS + Tailwind CSS 🔥
+		</h1>
+	</div>
+);
+export default App;
